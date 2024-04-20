d5394352d37b2e6e2707f9af395fd852051bd49a
Joao Lima <joaovictorbernardinodelima@gmail.com>
2024-04-20 09:53:15 -0300

    add Seller class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,18 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
+
         Department dep = new Department(1, "Books");
+        Seller seller = new Seller(1, "Joao Lima", "joao@gmail.com", new Date(), 2500.0, dep);
 
         System.out.println(dep);
+        System.out.println(seller);
     }
 }
