082367e64153e4efd1d099f6dc82e94472d698a9
Joao Lima <joaovictorbernardinodelima@gmail.com>
2024-04-20 13:02:26 -0300

    add findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,7 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -9,10 +11,9 @@ import java.util.Date;
 public class Program {
     public static void main(String[] args) {
 
-        Department dep = new Department(1, "Books");
-        Seller seller = new Seller(1, "Joao Lima", "joao@gmail.com", new Date(), 2500.0, dep);
+        SellerDao sellerDao = DaoFactory.createSellerDao();
+        Seller seller = sellerDao.findById(3);
 
-        System.out.println(dep);
         System.out.println(seller);
     }
 }
