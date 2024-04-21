5456aa4197a3f37fa967ea978bdaecb44fb3278b
Joao Lima <joaovictorbernardinodelima@gmail.com>
2024-04-21 18:36:54 -0300

    add DepartmentDaoJDBC

--- src/application/Program.java
+++ src/application/Program.java
@@ -8,9 +8,11 @@ import model.entities.Seller;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
         System.out.println("--- TESTE 1: seller findByID ---");
@@ -34,5 +36,19 @@ public class Program {
         Seller newSeller  = new Seller(null, "Maria Sofia", "mariasofia@gmail.com", new Date(), 4000.0, department);
         sellerDao.insert(newSeller);
         System.out.println("Insert new id:  = " + newSeller.getId());
+
+        System.out.println("\n--- TESTE 5: seller update ---");
+        seller = sellerDao.findById(1);
+        seller.setName("Maria Alice");
+        sellerDao.update(seller);
+        System.out.println("update complete!");
+
+        System.out.println("\n--- TESTE 6: seller delete ---");
+        System.out.println("Enter id for delete: ");
+        int id = sc.nextInt();
+        sellerDao.deleteById(id);
+        System.out.println("delete complete");
+
+        sc.close();
     }
 }
